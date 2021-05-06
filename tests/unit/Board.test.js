1c387334491da571e843f7760e26066b56c29444
Daniel Coughlan <daniel.coughlan85@gmail.com>
2021-05-06 18:58:38 +0100

    add tests folder

---
+++ tests/unit/Board.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/vue';
+import Board from '../../src/components/Board.vue';
+
+describe('Board.vue', () => {
+    it('Test basic render of Board', () => {
+        const title = 'Welcome to Simple BlackJack';
+        const { getByText } = render(Board, {
+            props: { title }
+        });
+        expect(getByText(msg)).toBeTruthy();
+    });
+});
