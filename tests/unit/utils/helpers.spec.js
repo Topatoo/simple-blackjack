fe3414f564371adb40ea265e8c97e105299059ec
Daniel Coughlan <daniel.coughlan85@gmail.com>
2021-05-07 08:27:47 +0100

    added unit tests for helpers

--- tests/unit/utils/helpers.spec.js
+++ tests/unit/utils/helpers.spec.js
@@ -1,5 +1,22 @@
+import { cardText, cardValue, createDeck } from '../../../src/utils/helpers';
+
 describe('helpers.js', () => {
-    it.todo('Test createDeck function');
-    it.todo('Test cardValue function');
-    it.todo('Test cardText function');
+    it('Test createDeck function', () => {
+        const deck = createDeck();
+        expect(deck.length).toBe(52);
+        expect(deck[0]).toStrictEqual({ suit: 'heart', value: 2, text: '2' });
+        expect(deck[12]).toStrictEqual({ suit: 'heart', value: 11, text: 'A' });
+    });
+    it('Test cardValue function', () => {
+        const valueA = cardValue(0);
+        expect(valueA).toStrictEqual(2);
+        const valueB = cardValue(12);
+        expect(valueB).toStrictEqual(11);
+    });
+    it('Test cardText function', () => {
+        const textA = cardText(0);
+        expect(textA).toStrictEqual('2');
+        const textB = cardText(12);
+        expect(textB).toStrictEqual('A');
+    });
 });
